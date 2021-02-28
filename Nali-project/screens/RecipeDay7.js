f4862473233778e5a6f9889170fcb2e14453f5bc
Christophe <christophe.sansouli@gmail.com>
2021-02-28 14:36:27 +0100

    Screen recipe day 7 ok

--- Nali-project/screens/RecipeDay7.js
+++ Nali-project/screens/RecipeDay7.js
@@ -2,11 +2,10 @@
 
 import React from 'react';
 import recipeDay4Photo from '../assets/soin4.jpg';
-import { StyleSheet, Text, View, ImageBackground, Image, ScrollView} from 'react-native';
-
+import { StyleSheet, ImageBackground, Image, ScrollView} from 'react-native';
+import { Button, Text, View} from 'native-base';
 import {  Header } from 'react-native-elements';
 import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
 
 import AppLoading from 'expo-app-loading';
 import { useFonts, Handlee_400Regular } from '@expo-google-fonts/handlee';
@@ -50,31 +49,32 @@ export default function RecipeDay1(props) {
    <View style={{ marginLeft: 25 }}>
    
 
-        <Text style={{ fontFamily: 'Roboto_500Medium', fontSize: 30, color: 'black', marginTop: 10, marginBottom: 10}}>Soin sans rinçage</Text>
+        <Text style={{ fontFamily: 'Roboto_500Medium', fontSize: 24, color: 'black', marginTop: 10, marginBottom: 10}}>Soin sans rinçage</Text>
         <Image style={{ flex: 1, width: 360 }} source={recipeDay4Photo}/>
         <ScrollView>
         <Text style={{ fontFamily: 'Roboto_700Bold', fontSize: 24, color: 'black', marginTop: 25, marginBottom: 10, fontWeight: 'bold' }}>Bienfaits</Text>
-        <Text style={{fontFamily: 'Roboto_400Regular'}}>L'objectif de cette étape est de conserver l'hydratation apportée en recouvrant vos cheveux d'une matière grasse ou huilde de votre choix. Cette méthode que les anglophanes appellent parfois L.O.C (liquide Oil Cream) permet d'obtenir d'excellents résultats</Text>
+        <Text style={{ fontFamily: 'Roboto_400Regular'}}>L'objectif de cette étape est de conserver l'hydratation apportée en recouvrant vos cheveux d'une matière grasse ou huilde de votre choix. Cette méthode que les anglophanes appellent parfois L.O.C (liquide Oil Cream) permet d'obtenir d'excellents résultats</Text>
         <Text style={{ fontFamily: 'Roboto_500Medium', fontSize: 20, color: 'black',  marginTop: 10, marginBottom: 10 }}>Ingrédients nécessaires</Text>
-        <Text style={{fontFamily: 'Roboto_400Regular'}}>- 2/3 d'eau de source</Text>
-        <Text style={{fontFamily: 'Roboto_400Regular'}}>- c-à-s de glycérine végétale</Text>
-        <Text style={{fontFamily: 'Roboto_400Regular'}}>- 1/3 de gel d'aloe vera</Text>
-        <Text style={{fontFamily: 'Roboto_400Regular'}}>- c-à-s d'une huile végétale de votre choix</Text>
+        <Text style={{ fontFamily: 'Roboto_400Regular'}}>- 2/3 d'eau de source</Text>
+        <Text style={{ fontFamily: 'Roboto_400Regular'}}>- c-à-s de glycérine végétale</Text>
+        <Text style={{ fontFamily: 'Roboto_400Regular'}}>- 1/3 de gel d'aloe vera</Text>
+        <Text style={{ fontFamily: 'Roboto_400Regular'}}>- c-à-s d'une huile végétale de votre choix</Text>
         <Text style={{ fontFamily: 'Roboto_500Medium', fontSize: 20, color: 'black',  marginTop: 15, marginBottom: 10 }}>Mode d'emploi</Text>
-        <Text style={{fontFamily: 'Roboto_400Regular'}}>Appliquez une fine couche d'huile végétale au choix puis une ou deux noisette du beurre végétale de votre choix sur l'ensemble de votre chevelure</Text>
-        <Text style={{ fontFamily: 'Roboto_500Medium', fontSize: 20, color: 'black', marginTop: 25, marginBottom: 5 }}>Terminé</Text>
+        <Text style={{ fontFamily: 'Roboto_400Regular'}}>Appliquez une fine couche d'huile végétale au choix puis une ou deux noisette du beurre végétale de votre choix sur l'ensemble de votre chevelure</Text>
+        
+         <Button dark
+          style={{marginTop: 35, marginBottom: 20, marginLeft: 120, backgroundColor: '#222222'}}
+          onPress={() => props.navigation.navigate( 'Bravo' ) }>
+           <Text style={{fontFamily: 'Roboto_500Medium', fontSize: 20}}> Terminé </Text>
+         </Button>
       
-        <View style={{alignItems: 'flex-end', marginBottom: 15, marginRight: 30}}>
-        <AntDesign 
-          onPress={() => props.navigation.navigate( 'Bravo' ) }
-          name="right" size={40} color="black" />
-        </View>
         </ScrollView>
                    
    </View>   
  </ImageBackground>
     
-    );}
+    );
+   }
   }
   
   const styles = StyleSheet.create({
