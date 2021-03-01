55d50c3d52bcd9d98d777dad125e292890d31f21
PhyllisU <phyllis_ugwonali@hotmail.fr>
2021-03-01 16:38:28 +0100

    ajout page shopping-list

--- Nali-project/screens/ShoppingList.js
+++ Nali-project/screens/ShoppingList.js
@@ -1,26 +1,168 @@
-// Phyllis
-import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+//OK ECRAN
+
+import React, { useState, useEffect} from 'react';
+import { StyleSheet, Text, ImageBackground, View} from 'react-native';
+import { Card, ListItem, CheckBox } from 'react-native-elements'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 
 export default function ShoppingList() {
-    return (
+
+
+  const [checkedBox1, setCheckedBox1] = useState(false);
+  const [checkedBox2, setCheckedBox2] = useState(false);
+  const [checkedBox3, setCheckedBox3] = useState(false);
+  const [checkedBox4, setCheckedBox4] = useState(false);
+
+ 
+  var handleSubmit1 = (check1) => {
+    setCheckedBox1(check1);
+    if(check1){
+      AsyncStorage.setItem("ingredient1", 'coché')
+    }else { 
+      AsyncStorage.setItem("ingredient1", 'pas coché')
+    }}
+   
+
+    var handleSubmit2 = (check2) => {
+      setCheckedBox2(check2);
+      if(check2){
+        AsyncStorage.setItem("ingredient2", 'coché')
+      }else { 
+        AsyncStorage.setItem("ingredient2", 'pas coché')
+      }}
+
+
+      var handleSubmit3 = (check3) => {
+        setCheckedBox3(check3);
+        if(check3){
+          AsyncStorage.setItem("ingredient3", 'coché')
+        }else { 
+          AsyncStorage.setItem("ingredient3", 'pas coché')
+        }}
+  
+  
+        var handleSubmit4 = (check4) => {
+          setCheckedBox4(check4);
+          if(check4){
+            AsyncStorage.setItem("ingredient4", 'coché')
+          }else { 
+            AsyncStorage.setItem("ingredient4", 'pas coché')
+          }}
+
+      useEffect(() => {
+       
+        AsyncStorage.getItem('ingredient1', (error, data) => {
+          if(data) {
+           if(data==='coché') {
+             setCheckedBox1(true) }
+               else {setCheckedBox1(false)
+                  { 
+             }
+           }
+          }
+        });
+
+        AsyncStorage.getItem('ingredient2', (error, data) => {
+          if(data) {
+           if(data==='coché') {
+             setCheckedBox2(true) }
+               else {setCheckedBox2(false)
+                  { 
+             }
+           }
+          }
+        });
+
+
+        AsyncStorage.getItem('ingredient3', (error, data) => {
+          if(data) {
+           if(data==='coché') {
+             setCheckedBox3(true) }
+               else {setCheckedBox3(false)
+                  { 
+             }
+           }
+          }
+        });
+
+
+        AsyncStorage.getItem('ingredient4', (error, data) => {
+          if(data) {
+           if(data==='coché') {
+             setCheckedBox4(true) }
+               else {setCheckedBox4(false)
+                  { 
+             }
+           }
+          }
+        });
+
+      }, []);
+
+
       
-  
-  <View>
-        <Text style={{ fontSize: 30, color: 'black', margin: 45 }}>Shopping List</Text>
-        
-         
-  </View>
-  
-     
-    );
-  }
-  
-  const styles = StyleSheet.create({
-     container: {
-       flex: 1,
-       alignItems: 'center',
-       justifyContent: 'flex-start',
-     },
-  });
+
+
+
+
+  return (
+    <ImageBackground source={require('../assets/002.png')}style={styles.container}> 
+
+      <View style={{alignItems: 'flex-start', marginTop: '10%'}}>
+   
+          <Card containerStyle={{width: '70%', borderRadius: 10}}>
+
+               <Text style={{fontSize: 30}}> Semaine 1</Text>
+
+                  <ListItem style={{alignItems: 'flex-start', marginBottom: -34, borderRadius: 20 }}>
+                    <CheckBox 
+                    checked={checkedBox1}
+                    checkedColor="#F475BB"
+                    onPress={() => handleSubmit1(!checkedBox1)}/>
+                    <Text>Huile d'olive</Text>      
+                  </ListItem >
+          
+                  <ListItem style={{alignItems: 'flex-start', marginBottom: -34}}>
+                    <CheckBox 
+                    checked={checkedBox2}
+                    checkedColor="#F475BB"
+                    onPress={() => handleSubmit2(!checkedBox2)}
+                    />
+                    <Text>Beurre de karité</Text>         
+                  </ListItem>
+               
+                  <ListItem style={{alignItems: 'flex-start', marginBottom: -34}}>
+                    <CheckBox 
+                    checked={checkedBox3}
+                    checkedColor="#F475BB"
+                    onPress={() => handleSubmit3(!checkedBox3)}/>
+                    <Text>Lait d'avoine</Text>         
+                  </ListItem>
+
+                  <ListItem style={{alignItems: 'flex-start', marginBottom: -10}}>
+                    <CheckBox 
+                    checked={checkedBox4}
+                    checkedColor="#F475BB"
+                    onPress={() => handleSubmit4(!checkedBox4)}
+                    />
+                    <Text>Avocat</Text>         
+                  </ListItem>
+
+          </Card>
+
+      </View>
+
+    </ImageBackground>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+});
